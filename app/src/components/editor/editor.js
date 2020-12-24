4990947fdfb384a475aaa96dfd92c068e930a1ff
niminskiy <s.niminskiy@gmail.com>
2020-12-24 18:33:11 +0300

    add backup

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -5,74 +5,90 @@ import DOMHelper from '../../helpers/dom-helper';
 import EditorText from '../editor-text';
 import UIkit from 'uikit';
 import Spinner from '../spinner';
+import ConfirmModal from '../confirm-modal';
+import ChooseModal from '../choose-modal';
+import Panel from '../panel';
 
 export default class Editor extends Component {
-	constructor() {
-		super();
-		this.currentPage = "index.html";
-		this.state = {
-			pageList: [],
-			newPageName: "",
-			loading: true
-		}
-		this.createNewPage = this.createNewPage.bind(this);
-		this.isLoading = this.isLoading.bind(this);
-		this.isLoaded = this.isLoaded.bind(this);
-	}
+    constructor() {
+        super();
+        this.currentPage = "index.html";
+        this.state = {
+            pageList: [],
+            backupsList: [],
+            newPageName: "",
+            loading: true
+        }
+        this.isLoading = this.isLoading.bind(this);
+        this.isLoaded = this.isLoaded.bind(this);
+        this.save = this.save.bind(this);
+        this.init = this.init.bind(this);
+        this.restoreBackup = this.restoreBackup.bind(this);
+    }
 
-	componentDidMount() {
-		this.init(this.currentPage);
-	}
+    componentDidMount() {
+        this.init(null, this.currentPage);
+    }
 
-	init(page) {
-		this.iframe = document.querySelector('iframe');
-		this.open(page, this.isLoaded);
-		this.loadPageList();
-	}
+    init(e, page) {
+        if (e) {
+            e.preventDefault();
+        }
+        this.isLoading();
+        this.iframe = document.querySelector('iframe');
+        this.open(page, this.isLoaded);
+        this.loadPageList();
+        this.loadBackupsList();
+    }
 
-	open(page, cb) {
-		this.currentPage = page;
+    open(page, cb) {
+        this.currentPage = page;
 
-		axios
-			.get(`../${page}?rnd=${Math.random()}`)
-			.then(res => DOMHelper.parseStrToDOM(res.data))
-			.then(DOMHelper.wrapTextNodes)
-			.then(dom => {
-				this.virtualDom = dom;
-				return dom;
-			})
-			.then(DOMHelper.serializeDOMToString)
-			.then(html => axios.post("./api/saveTempPage.php", { html }))
-			.then(() => this.iframe.load("../temp.html"))
-			.then(() => this.enableEditing())
-			.then(() => this.injectStyles())
-			.then(cb);
-	}
+        axios
+            .get(`../${page}?rnd=${Math.random()}`)
+            .then(res => DOMHelper.parseStrToDOM(res.data))
+            .then(DOMHelper.wrapTextNodes)
+            .then(dom => {
+                this.virtualDom = dom;
+                return dom;
+            })
+            .then(DOMHelper.serializeDOMToString)
+            .then(html => axios.post("./api/saveTempPage.php", { html }))
+            .then(() => this.iframe.load("../yfuy1g221ub_hhg44.html"))
+            .then(() => axios.post("./api/deleteTempPage.php"))
+            .then(() => this.enableEditing())
+            .then(() => this.injectStyles())
+            .then(cb);
 
-	save(onSuccess, onError) {
-		this.isLoading();
-		const newDom = this.virtualDom.cloneNode(this.virtualDom);
-		DOMHelper.unwrapTextNodes(newDom);
-		const html = DOMHelper.serializeDOMToString(newDom);
-		axios
-			.post("./api/savePage.php", { pageName: this.currentPage, html })
-			.then(onSuccess)
-			.catch(onError)
-			.finally(this.isLoaded);
-	}
+        this.loadBackupsList();
+    }
 
-	enableEditing() {
-		this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
-			const id = element.getAttribute("nodeid");
-			const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+    async save(onSuccess, onError) {
+        this.isLoading();
+        const newDom = this.virtualDom.cloneNode(this.virtualDom);
+        DOMHelper.unwrapTextNodes(newDom);
+        const html = DOMHelper.serializeDOMToString(newDom);
+        await axios
+            .post("./api/savePage.php", { pageName: this.currentPage, html })
+            .then(onSuccess)
+            .catch(onError)
+            .finally(this.isLoaded);
 
-			new EditorText(element, virtualElement);
-		});
-	}
+        this.loadBackupsList();
+    }
 
-	injectStyles() {
-		const style = this.iframe.contentDocument.createElement("style");
-		style.innerHTML = `
+    enableEditing() {
+        this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
+            const id = element.getAttribute("nodeid");
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+
+            new EditorText(element, virtualElement);
+        });
+    }
+
+    injectStyles() {
+        const style = this.iframe.contentDocument.createElement("style");
+        style.innerHTML = `
             text-editor:hover {
                 outline: 3px solid orange;
                 outline-offset: 8px;
@@ -82,77 +98,73 @@ export default class Editor extends Component {
                 outline-offset: 8px;
             }
         `;
-		this.iframe.contentDocument.head.appendChild(style);
-	}
+        this.iframe.contentDocument.head.appendChild(style);
+    }
 
-	loadPageList() {
-		axios
-			.get("./api")
-			.then(res => this.setState({ pageList: res.data }))
-	}
+    loadPageList() {
+        axios
+            .get("./api/pageList.php")
+            .then(res => this.setState({ pageList: res.data }))
+    }
 
-	createNewPage() {
-		axios
-			.post("./api/createNewPage.php", { "name": this.state.newPageName })
-			.then(this.loadPageList())
-			.catch(() => alert("Страница уже существует!"));
-	}
+    loadBackupsList() {
+        axios
+            .get("./backups/backups.json")
+            .then(res => this.setState({
+                backupsList: res.data.filter(backup => {
+                    return backup.page === this.currentPage;
+                })
+            }))
+    }
 
-	deletePage(page) {
-		axios
-			.post("./api/deletePage.php", { "name": page })
-			.then(this.loadPageList())
-			.catch(() => alert("Страницы не существует!"));
-	}
+    restoreBackup(e, backup) {
+        if (e) {
+            e.preventDefault();
+        }
+        UIkit.modal.confirm("Вы действительно хотите восстановить страницу из этой резервной копии? Все несохраненные данные будут потеряны!", { labels: { ok: 'Восстановить', cancel: 'Отмена' } })
+            .then(() => {
+                this.isLoading();
+                return axios
+                    .post('./api/restoreBackup.php', { "page": this.currentPage, "file": backup })
+            })
+            .then(() => {
+                this.open(this.currentPage, this.isLoaded);
+            })
+    }
 
-	isLoading() {
-		this.setState({
-			loading: true
-		})
-	}
+    isLoading() {
+        this.setState({
+            loading: true
+        })
+    }
 
-	isLoaded() {
-		this.setState({
-			loading: false
-		})
-	}
+    isLoaded() {
+        this.setState({
+            loading: false
+        })
+    }
 
-	render() {
-		const { loading } = this.state;
-		const modal = true;
-		let spinner;
+    render() {
+        const { loading, pageList, backupsList } = this.state;
+        const modal = true;
+        let spinner;
 
-		loading ? spinner = <Spinner active /> : spinner = <Spinner />
+        console.log(backupsList);
 
-		return (
-			<>
-				<iframe src={this.currentPage} frameBorder="0"></iframe>
+        loading ? spinner = <Spinner active /> : spinner = <Spinner />
 
-				{spinner}
+        return (
+            <>
+                <iframe src="" frameBorder="0"></iframe>
 
-				<div className="panel">
-					<button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
-				</div>
+                {spinner}
 
-				<div id="modal-save" uk-modal={modal.toString()} container="false">
-					<div className="uk-modal-dialog uk-modal-body">
-						<h2 className="uk-modal-title">Сохранение</h2>
-						<p>Вы действительно хотите сохранить изменения?</p>
-						<p className="uk-text-right">
-							<button className="uk-button uk-button-default uk-modal-close" type="button">Отменить</button>
-							<button
-								className="uk-button uk-button-primary uk-modal-close"
-								type="button"
-								onClick={() => this.save(() => {
-									UIkit.notification({ message: 'Успешно сохранено', status: 'success' })
-								},
-									() => {
-										UIkit.notification({ message: 'Ошибка сохранения', status: 'danger' })
-									})}>Опубликовать</button>
-						</p>
-					</div>
-				</div>
-			</>
-		)
-	}
+                <Panel />
+
+                <ConfirmModal modal={modal} target={'modal-save'} method={this.save} />
+                <ChooseModal modal={modal} target={'modal-open'} data={pageList} redirect={this.init} />
+                <ChooseModal modal={modal} target={'modal-backup'} data={backupsList} redirect={this.restoreBackup} />
+            </>
+        )
+    }
 }
